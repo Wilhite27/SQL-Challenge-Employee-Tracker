c04a833da2204567f4babf0b17238a7eaea77a2b
Dwilhite27 <wilhitedurrelle@gmail.com>
2021-07-21 16:27:14 -0500

    inquirer-err-rough-draft

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,9 +9,9 @@ INSERT INTO roles (title, salary, department_id) VALUES ("Data Analyst","145000"
 INSERT INTO roles (title, salary, department_id) VALUES ("Mangement Director","55000","3");
 INSERT INTO roles (title, salary, department_id) VALUES ("Custodial Specialist","45000","4");
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id, manager_name) VALUES ("Homer", "Simpson", 2/* , "Larry","Burns"*/);
-INSERT INTO employees (first_name, last_name, role_id, manager_id, manager_name) VALUES ("Marge", "Simpson", 1/*, "Freddy","Pickles"*/);
-INSERT INTO employees (first_name, last_name, role_id, manager_id, manager_name) VALUES ("Bart", "Simpson", 4/*,"Tabitha","Doubtfire"*/);
-INSERT INTO employees (first_name, last_name, role_id, manager_id, manager_name) VALUES ("Lisa", "Simpson", 3/*, "Joanna", "Krabappel"*/);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Homer", "Simpson", 2/* , "Larry","Burns"*/);
+INSERT INTO employees (first_name, last_name, role_id, manager_id ) VALUES ("Marge", "Simpson", 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id ) VALUES ("Bart", "Simpson", 4);
+INSERT INTO employees (first_name, last_name, role_id, manager_id ) VALUES ("Lisa", "Simpson", 3);
 
 
