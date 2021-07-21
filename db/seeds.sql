1c8ea08c2f4179ea123c3f6bdc1062102cb8eb2d
Dwilhite27 <wilhitedurrelle@gmail.com>
2021-07-21 16:31:42 -0500

    inquirer-err-rough-draft2

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,8 +9,8 @@ INSERT INTO roles (title, salary, department_id) VALUES ("Data Analyst","145000"
 INSERT INTO roles (title, salary, department_id) VALUES ("Mangement Director","55000","3");
 INSERT INTO roles (title, salary, department_id) VALUES ("Custodial Specialist","45000","4");
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Homer", "Simpson", 2);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Marge", "Simpson", 1);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Bart", "Simpson", 4);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Lisa", "Simpson", 3);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Homer", "Simpson", 2, null);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Marge", "Simpson", 1, null;
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Bart", "Simpson", 4, null);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Lisa", "Simpson", 3, null);
 
